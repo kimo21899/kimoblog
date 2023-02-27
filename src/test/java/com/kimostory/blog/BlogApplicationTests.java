0bd63ea78e7e96de03acc6233b408df9952e7974
kimo21899 <lkm24@naver.com>
2023-02-28 08:36:29 +0900

    Initial commit

---
+++ src/test/java/com/kimostory/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimostory.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
